c267c90dcd5a767c76ac018350184c917f2b2637
traducha <traducha@vayandata.com>
2023-05-04 10:12:36 +0200

    feat(fetchAllRooms): Adding new function

--- src/Util/utilsApi.js
+++ src/Util/utilsApi.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-async function fetchRoomData(selectedRoomIds) {
-  const username = import.meta.env.VITE_USERNAME;
-  const password = import.meta.env.VITE_PASSWORD;
-  const authString = username + ":" + password;
-  const encodedAuthString = btoa(authString);
+const username = import.meta.env.VITE_USERNAME;
+const password = import.meta.env.VITE_PASSWORD;
+const authString = username + ":" + password;
+const encodedAuthString = btoa(authString);
 
+async function fetchRoomData(selectedRoomIds) {
   const response = await Promise.all(
     selectedRoomIds.map((roomId) =>
       axios.get(
@@ -23,11 +23,6 @@ async function fetchRoomData(selectedRoomIds) {
 }
 
 async function fetchAllRoom() {
-  const username = import.meta.env.VITE_USERNAME;
-  const password = import.meta.env.VITE_PASSWORD;
-  const authString = username + ":" + password;
-  const encodedAuthString = btoa(authString);
-
   try {
     const response = await axios.get(
       `https://192.168.12.146:443/v2/history?retrieveValues=true&period=lastYear`,
@@ -38,7 +33,6 @@ async function fetchAllRoom() {
         },
       }
     );
-
     const spaces = response.data
       .map((element) => {
         return element.space !== null && element.space !== undefined
@@ -53,4 +47,66 @@ async function fetchAllRoom() {
   }
 }
 
-export { fetchRoomData, fetchAllRoom };
+async function fetchAllRoomId() {
+  try {
+    const response = await axios.get(
+      `https://192.168.12.146:443/v2/history?retrieveValues=true&period=lastYear`,
+      {
+        headers: {
+          mode: "cors",
+          Authorization: "Basic " + encodedAuthString,
+        },
+      }
+    );
+
+    const spacesId = response.data
+      .map((element) => {
+        return element.spaceId !== null && element.spaceId !== undefined
+          ? element.spaceId
+          : null;
+      })
+      .filter((spaceId) => spaceId !== null);
+
+    return spacesId;
+  } catch (error) {
+    console.error("Erreur lors de la récupération des données:", error);
+  }
+}
+
+async function fetchAllRooms() {
+  try {
+    const response = await axios.get(
+      `https://192.168.12.146:443/v2/history?retrieveValues=true&period=lastYear`,
+      {
+        headers: {
+          mode: "cors",
+          Authorization: "Basic " + encodedAuthString,
+        },
+      }
+    );
+
+    const rooms = response.data
+      .map((element) => {
+        const space =
+          element.space !== null && element.space !== undefined
+            ? element.space
+            : null;
+        const spaceId =
+          element.spaceId !== null && element.spaceId !== undefined
+            ? element.spaceId
+            : null;
+
+        if (space !== null && spaceId !== null) {
+          return { space, spaceId };
+        } else {
+          return null;
+        }
+      })
+      .filter((room) => room !== null);
+    return rooms;
+  } catch (error) {
+    console.error("Erreur lors de la récupération des données:", error);
+  }
+}
+
+export { fetchRoomData, fetchAllRoom, fetchAllRoomId, fetchAllRooms };
